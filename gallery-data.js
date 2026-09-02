9b146d0f1645261f1f4c376e9b1304192f6f138d
vincentozzomondi1997-cloud <vincentozzomondi1997@gmail.com>
2026-09-02 08:48:01 -0700

    Create gallery-data.js

---
+++ gallery-data.js
@@ -0,0 +1,39 @@
+const galleryData = [
+  {
+    file: "bar.jpg",
+    alt: "Mwisho Tourist Hotel bar",
+    caption: "Hotel Bar"
+  },
+  {
+    file: "exterior.jpg",
+    alt: "Mwisho Tourist Hotel exterior",
+    caption: "Hotel Exterior"
+  },
+  {
+    file: "grounds.jpg",
+    alt: "Mwisho Tourist Hotel grounds",
+    caption: "Hotel Grounds"
+  },
+  {
+    file: "hotel-grounds-1.jpg",
+    alt: "Mwisho Tourist Hotel grounds",
+    caption: "Hotel Grounds"
+  },
+  {
+    file: "hotel-grounds-2.jpg",
+    alt: "Mwisho Tourist Hotel grounds",
+    caption: "Hotel Grounds"
+  },
+  {
+    file: "hotel-van.jpg",
+    alt: "Mwisho Tourist Hotel transport",
+    caption: "Hotel Transport"
+  },
+  {
+    file: "lounge.jpg",
+    alt: "Mwisho Tourist Hotel lounge",
+    caption: "Hotel Lounge"
+  }
+];
+
+window.MWISHO_GALLERY = galleryData;
